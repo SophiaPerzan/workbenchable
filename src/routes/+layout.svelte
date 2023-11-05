d2a451236f8f49c3bf7a89a81d2055760c5ac8c6
Sophia <sophia@perzan.dev>
2023-11-05 01:53:06 -0400

    setup tailwind and daisyui

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<div class="flex flex-col min-h-dynamic-screen">
+	<nav />
+	<main class="flex-grow flex flex-col items-center gap-4 mx-1/16">
+		<slot />
+	</main>
+	<footer />
+</div>
+
+<style>
+	.min-h-dynamic-screen {
+		min-height: 100dvh;
+	}
+</style>
